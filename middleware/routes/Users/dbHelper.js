7fbae752e2ad74042b65736d528f728914cf4ae0
joepound <14357797+joepound@users.noreply.github.com>
2019-03-11 15:06:43 -0600

    Added data access method to get user info

--- middleware/routes/Users/dbHelper.js
+++ middleware/routes/Users/dbHelper.js
@@ -1,9 +1,20 @@
 const db = require("../../../data/dbConfig");
 
 module.exports = {
-  registerUser
+  registerUser,
+  getUsers
 };
 
 function registerUser(userData) {
   return db("Users").insert(userData);
+}
+
+function getUsers(user) {
+  if (user) {
+    return db("Users")
+      .select("UserID", "UserName")
+      .where({ UserID: user.UserID })
+      .first();
+  }
+  return db("Users").select("UserID", "UserName");
 }
