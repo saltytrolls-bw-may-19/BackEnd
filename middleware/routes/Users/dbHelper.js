c2cfa55e9b8362a85da88616e6e0afdc0882dd93
joepound <14357797+joepound@users.noreply.github.com>
2019-03-12 14:13:42 -0600

    Added data access functions for deleting user and updating user password

--- middleware/routes/Users/dbHelper.js
+++ middleware/routes/Users/dbHelper.js
@@ -2,18 +2,32 @@ const db = require("../../../data/dbConfig");
 
 module.exports = {
   registerUser,
-  getUsers
+  getUsers,
+  deleteUser,
+  updateUserPassword
 };
 
-function registerUser(userData) {
-  return db("Users").insert(userData);
+function registerUser(userInfo) {
+  return db("Users").insert(userInfo);
 }
 
 function getUsers(user) {
   if (user) {
     return db("Users")
-      .where({ "UserEmail": user.UserEmail })
+      .where({ UserEmail: user.UserEmail })
       .first();
   }
   return db("Users").select("UserID", "UserEmail");
+}
+
+function deleteUser(UserID) {
+  return db("Users")
+    .delete()
+    .where({ UserID });
+}
+
+function updateUserPassword(UserID, UserPassword) {
+  return db("Users")
+    .update({ UserPassword })
+    .where({ UserID });
 }
