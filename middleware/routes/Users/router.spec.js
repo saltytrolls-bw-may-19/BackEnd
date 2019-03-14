fae35ffb830b220d959b8470755d2390bdd71666
joepound <14357797+joepound@users.noreply.github.com>
2019-03-14 09:53:02 -0600

    Added faker.js for generating test data

--- middleware/routes/Users/router.spec.js
+++ middleware/routes/Users/router.spec.js
@@ -1,9 +1,13 @@
 const request = require("supertest");
 const server = require("../../../server.js");
+const faker = require("faker");
 
 const db = require("../../../data/dbConfig.js");
 const dbHelper = require("./dbHelper.js");
 
 describe("Users routes:", () => {
-
+  const testUsers = Array(3).fill().map(user => ({
+    UserEmail: faker.internet.email(),
+    UserPassword: faker.internet.password()
+  }));
 });
