051cd4f68896ffeb0b252ba747c19a2703dd84d6
joepound <14357797+joepound@users.noreply.github.com>
2019-03-14 15:13:30 -0600

    Added tests for user login endpoint

--- middleware/routes/Users/router.spec.js
+++ middleware/routes/Users/router.spec.js
@@ -7,6 +7,7 @@ const dbHelper = require("./dbHelper.js");
 const bcrypt = require("bcrypt");
 
 describe("Users routes:", () => {
+  // Generate dummy data (users) for testing
   const testUsers = Array(3)
     .fill()
     .map(user => ({
@@ -14,9 +15,11 @@ describe("Users routes:", () => {
       UserPassword: faker.internet.password()
     }));
 
+  // Quick function for inserting all test users
   const insertAllUsers = () =>
     testUsers.forEach(user => (async () => await dbHelper.insert(user))());
 
+  // Clear the test DB after every test
   afterEach(() => db("Users").truncate());
 
   describe(`Request to "POST /api/users/register":`, () => {
@@ -53,6 +56,58 @@ describe("Users routes:", () => {
       expect(
         bcrypt.compareSync(testUsers[0].UserPassword, addedUser.UserPassword)
       ).toBeTruthy();
+    });
+
+    it("• should return status 422 upon sending incomplete information", async () => {
+      const res = await request(server)
+        .post(reqURL)
+        .send({});
+      expect(res.status).toBe(422);
+    });
+  });
+
+  describe(`Request to "POST /api/users/login":`, () => {
+    const reqURL = "/api/users/login";
+
+    it("• should return a JSON", async () => {
+      const res = await request(server).post(reqURL);
+      expect(res.type).toBe("application/json");
+    });
+
+    it("• should return status 200 upon sending correct credentials", async () => {
+      // Register the user first to ensure a valid login
+      await request(server)
+        .post("/api/users/register")
+        .send(testUsers[0]);
+
+      const res = await request(server)
+        .post(reqURL)
+        .send(testUsers[0]);
+      expect(res.status).toBe(200);
+    });
+
+    it("• should return a JSON with the corresponding UserID, UserEmail and token", async () => {
+      // Add the users first to ensure a valid login
+      await request(server)
+        .post("/api/users/register")
+        .send(testUsers[0]);
+
+      const res = await request(server)
+        .post(reqURL)
+        .send(testUsers[0]);
+      expect(res.body).toEqual({
+        UserID: 1,
+        UserEmail: testUsers[0].UserEmail,
+        token: res.body.token
+      });
+    });
+
+    it("• should return status 401 upon sending incorrect credentials", async () => {
+      // Simply omit registration here
+      const res = await request(server)
+        .post(reqURL)
+        .send(testUsers[0]);
+      expect(res.status).toBe(401);
     });
 
     it("• should return status 422 upon sending incomplete information", async () => {
